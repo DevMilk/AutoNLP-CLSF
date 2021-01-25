b40e24303270174851149eda9e973fe2e4d973dc
DevMilk <ugurkk2599@gmail.com>
2021-01-25 23:26:40 +0300

    fix session compability

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template,session
 from flask_dropzone import Dropzone
 from io import StringIO
 import os
 from flask_session import Session
 from flask_caching import Cache
-from models.model_tree_functions import defineData, buildTreeByParamTree,runMethodOfModel, simplifyTree,\
+from .models.model_tree_functions import defineData, buildTreeByParamTree,runMethodOfModel, simplifyTree,\
                                             get_train_test_dataset
-from models.model_tree import *
+from .models.model_tree import *
 app = Flask(__name__, template_folder='templates')
 
 
@@ -18,10 +18,12 @@ config = {
     "DROPZONE_ALLOWED_FILE_CUSTOM": True,
     "DROPZONE_ALLOWED_FILE_TYPE": '.csv, .xlsx, .xls',
     "CACHE_TYPE": "simple", # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 0
+    "CACHE_DEFAULT_TIMEOUT": 0,
+    "secret_key": "super secret",
+    "SESSION_TYPE": "filesystem"
 }
 app.config.from_mapping(config)
-session =  Session(app)
+sess = Session(app)
 cache = Cache(app)
 dropzone = Dropzone(app)
 
@@ -40,12 +42,11 @@ def hello():
 @app.route('/model-tree')
 @cache.cached(timeout=120)
 def getStructure():
-    try:
-        session.model_tree
-    except:
-        session.X_train,session.X_test,session.y_train,session.y_test,session.cleaned_data = defineData(data_path)
-        session.model_tree = buildTreeByParamTree(param_tree)
-    return jsonify(simplifyTree(session.model_tree))
+    model_tree = session.get("model_tree")
+    if(model_tree==None):
+        session["X_train"], session["X_test"], session["y_train"], session["y_test"],session["cleaned_data"] = defineData(data_path)
+        session["model_tree"] = buildTreeByParamTree(param_tree)
+    return jsonify(simplifyTree(session.get("model_tree")))
 
 #------------------------------METHOD ENDPOINTS----------------------------
 
@@ -55,7 +56,7 @@ def getStructure():
 def get_param(): 
     parameters = request.get_json()
     args = parameters.get("args")
-    return jsonify(runMethodOfModel("get_params", args,(),session.model_tree))
+    return jsonify(runMethodOfModel("get_params", args,(),session.get("model_tree")))
 
 
 #Split Dataset
@@ -64,7 +65,7 @@ def change_split():
     try:
         parameters = request.get_json()
         test_ratio = parameters.get("test_ratio")
-        session.X_train, session.X_test, session.y_train, session.y_test = get_train_test_dataset(session.cleaned_data,test_ratio)
+        session["X_train"], session["X_test"], session["y_train"], session["y_test"] = get_train_test_dataset(session.get("cleaned_data"),test_ratio)
         return jsonify(["Data Splitted with {} test data ratio".format(test_ratio)])
     except Exception as e:
         print(e)
@@ -78,7 +79,7 @@ def predict():
         parameters = request.get_json()
         text = parameters.get("text")
         args = parameters.get("args")
-        return jsonify(runMethodOfModel("predict",args,[text],session.model_tree))
+        return jsonify(runMethodOfModel("predict",args,[text],session.get("model_tree")))
     except Exception as e:
         print("Error "+str(e))
         return jsonify(["Model Not Trained"])
@@ -90,7 +91,7 @@ def test():
     parameters = request.get_json()
     args = parameters.get("args")
     try:
-        return jsonify(runMethodOfModel("evaluate", args, (session.X_test,session.y_test),session.model_tree))
+        return jsonify(runMethodOfModel("evaluate", args, (session.get("X_test"),session.get("y_test")),session.get("model_tree")))
     except:
         return jsonify(["Model Not Trained"])
 
@@ -102,9 +103,12 @@ def train():
         parameters = request.get_json()
         args = parameters.get("args")
         params = parameters.get("params") #must be a dict
+        temp_tree = session.get("model_tree")
         if(params!=None):
-            runMethodOfModel("set_params_of_model",args,[(params)],session.model_tree)
-        runMethodOfModel("fit",args,(session.X_train,session.y_train),session.model_tree)
+            runMethodOfModel("set_params_of_model",args,[(params)],temp_tree)
+        runMethodOfModel("fit",args,(session.get("X_train"),session.get("y_train")),temp_tree)
+
+        session["model_tree"] = temp_tree
         return jsonify(["Training Success"])
     except Exception as e:
         print(e)
@@ -117,7 +121,7 @@ def setDataset():
         print("uploading...")
         if request.method == 'POST':
             data_file = request.files.get("file[0]")
-            session.X_train,session.X_test,session.y_train,session.y_test,session.cleaned_data = \
+            session["X_train"], session["X_test"], session["y_train"], session["y_test"],session["cleaned_data"] = \
                 defineData(StringIO(data_file.read().decode("utf-8")))
         return jsonify(["UPLOAD SUCCESSFULL"])
     except Exception as e:
@@ -128,4 +132,4 @@ def setDataset():
 #--------------------------------------------------------------------------
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
